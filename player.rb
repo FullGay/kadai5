b946038d65e706132b4d4cf1bb1613812424c2e3
unknown <cosmos_885@icloud.com>
2017-10-04 00:38:11 +0900

    playerクラス変更

--- player.rb
+++ player.rb
@@ -1,8 +1,22 @@
 # coding: utf-8
 
 class Player < Sprite
+  @isHit = false
+
+  def isHit=(bool)
+    @isHit = bool
+  end
+
+  def isHit
+    @isHit
+  end
+
+  def shot
+    self.isHit=true
+  end
+  
   def update
-    self.x += Input.x
-    self.y += Input.y
+    self.x += 3*Input.x
+    self.y += 3*Input.y
   end
 end
