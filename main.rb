c1dfbb0853120dfacf113c227bd4f33650920dff
unknown <cosmos_885@icloud.com>
2017-10-04 00:30:27 +0900

    first commit

--- main.rb
+++ main.rb
@@ -13,22 +13,45 @@ player_img.setColorKey([0, 0, 0])
 enemy_img = Image.load("enemy.png")
 enemy_img.setColorKey([0, 0, 0])
 
-player = Player.new(400, 500, player_img)
+StartWin = Image.load("StartWindow.png")
+Back = Image.load("BackGround.png")
+GameOverWin = Image.load("GameOverWindow.png")
 
+player = Player.new(400, 500, player_img)
+isStart = false
+isGameOver = false
 enemies = []
-10.times do
-  enemies << Enemy.new(rand(800), rand(600), enemy_img)
-end
 
 Window.loop do
   break if Input.keyPush?(K_ESCAPE)
+  if Input.keyPush?(K_A) && !isStart
+    isStart = true 
+    isGameOver = false
+    10.times do |i|
+      enemies[i] =  Enemy.new(rand(800), rand(600), enemy_img)
+      player.isHit=false
+    end
+  end
+  if player.isHit
+    isGameOver = true
+    isStart = false
+  end
 
+  if !isStart && !isGameOver
+    Window.draw(0,0,StartWin)
+    next
+  end
+  if isGameOver && !isStart
+    break if Input.keyPush?(K_ESCAPE)
+    Window.draw(0,0,GameOverWin)
+    next
+  end
+  Window.draw(0,0,Back)
   Sprite.update(enemies)
   Sprite.draw(enemies)
 
   player.update
   player.draw
-
   # 当たり判定
   Sprite.check(player, enemies)
 end
